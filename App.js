93d1103496eb82476f232f05ba7c92c568b35ab5
afmssrahi <afmssrahi@gmail.com>
2022-05-14 04:44:56 +0600

    setup react-native project

--- App.js
+++ App.js
@@ -2,19 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>Hello World!</Text>
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+	},
+	text: {
+		fontSize: 30,
+		fontWeight: 'bold',
+		color: '#06bcee',
+		marginTop: 30,
+		backgroundColor: '#bdbdbd',
+		paddingHorizontal: 20,
+		paddingVertical: 40,
+	},
 });
